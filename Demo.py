368e084957a22ca14efd679dd8e14b2ec35e5e10
Silvio <silvio@Macmini.local>
2020-02-18 22:16:34 +0800

    Add WaitTime Function

--- Demo.py
+++ Demo.py
@@ -4,6 +4,7 @@ import os
 import time
 import csv
 
+
 def write_record_csv(reps, sets, course, duration):
     path = 'exerice_recording.csv'
     with open(path, 'a', newline='', encoding='utf-8-sig') as file:
@@ -12,11 +13,11 @@ def write_record_csv(reps, sets, course, duration):
         data = [time.time(), time.strftime('%Y-%m-%d %H:%M:%S'), course, reps, sets, duration]
         csv_file.writerow(data)
 
-def StartTrain(reps: int = 25, sets: int = 1, m: int = 1, w: int = 1, remind: int = 10, breaktime: float = 1, course: str = '自由练习'):
+
+def StartTrain(reps: int = 25, sets: int = 1, m: int = 1, w: int = 1, remind: int = 10, breaktime: float = 1,
+               course: str = '自由练习'):
     try:
-        # Keyboard_Listener(on_press, on_release)
         os.system('say "现在开始"')
-        # time.sleep(5)
         global starttime
         starttime = time.time()
         global r
@@ -53,7 +54,7 @@ def StartTrain(reps: int = 25, sets: int = 1, m: int = 1, w: int = 1, remind: in
         print('ERR')
         duration = round((time.time() - starttime) / 60, 1)
         print(duration)
-        write_record_csv(reps=reps * r + t, sets= 1, course=course, duration=duration)
+        write_record_csv(reps=reps * r + t, sets=1, course=course, duration=duration)
 
 
     except Exception as err:
@@ -61,39 +62,43 @@ def StartTrain(reps: int = 25, sets: int = 1, m: int = 1, w: int = 1, remind: in
     finally:
         print('Done')
 
+
+def WaitTime(t1=0):
+    if t1 == 0:
+        t = input("请输入等待开始时间(默认为5秒):")
+        if type(t) != int or t == "":
+            t1 = 5
+    time.sleep(t1)
+
+
 if __name__ == '__main__':
-    time.sleep(5)
-    #俯卧撑系列
-    # StartTrain(20, 3, 1, 1, 10, 0.5, '斜上俯卧撑')
-    StartTrain(12, 2, 1, 1, 10, 0.5, '标准俯卧撑')
-    # StartTrain(20, 2, 1, 1, 10, 0.5, '窄距俯卧撑')
-    # StartTrain(20, 2, 1, 1, 10, 0.5, '单臂俯卧撑')
-    #深蹲系列
-    # StartTrain(20, 2, 1, 1, 10, 0.5, '标准深蹲')
-    # StartTrain(20, 2, 1, 1, 10, 0.5, '窄距深蹲')
-    # StartTrain(20, 2, 1, 1, 10, 0.5, '单腿深蹲')
-    #引体向上
-    # StartTrain(10, 2, 1, 1, 10, 0.5, '标准引体向上')
-    # StartTrain(10, 2, 1, 1, 10, 0.5, '窄距引体向上')
-    # StartTrain(6, 2, 1, 1, 10, 0.5, '单臂引体向上')
-    #举腿系列
+    WaitTime(2)
+    print('done')
+    # 俯卧撑系列
+    # StartTrain(20, 3, 1, 1, 10, 0.5, '斜上俯卧撑')  #标准俯卧撑2-12 窄距俯卧撑 #单臂俯卧撑
+
+    # 深蹲系列
+    # StartTrain(20, 4, 1, 1, 10, 0.5, '标准深蹲') #窄距深蹲 单腿深蹲
+
+    # 引体向上
+    # StartTrain(10, 2, 1, 1, 10, 0.5, '标准引体向上') #窄距引体向上') 单臂引体向上2-10
+
+    # 举腿系列
     # StartTrain(20, 2, 1, 1, 10, 0.5, '平卧抬膝')  #平卧抬膝3-35   平卧屈举腿3-30   平卧蛙举腿3-25   平卧直举腿2-20
-    # StartTrain(15, 2, 1, 1, 10, 0.5, '悬垂屈膝')  #悬垂抬膝   悬垂屈举腿   悬垂蛙举腿   悬垂直举腿
-    # StartTrain(30, 2, 1, 1, 10, 0.5, '悬垂直举腿')
+    # StartTrain(15, 2, 1, 1, 10, 0.5, '悬垂屈膝')  #悬垂抬膝   悬垂屈举腿   悬垂蛙举腿   悬垂直举腿2-30
 
-    #桥系列
+    # 桥系列
     # StartTrain(30, 4, 1, 1, 10, 0.5, '短桥')    #直桥3-40 高低桥3-30 顶桥2-25  半桥2-20  标准桥2-15 下行桥2-10 上行桥2-8  合桥2-6   铁板桥2-30
-    #倒立系列
 
+    # 倒立系列
+    '''
     # Todo
     #   StartTrain(10, 2, 1, 1, 10, 0.5, '靠墙顶立') 2分钟  乌鸦式1分钟  靠墙倒立2分钟
 
     # StartTrain(20, 2, 1, 1, 10, 0.5, '半倒立撑')
     # StartTrain(15, 2, 1, 1, 10, 0.5, '标准倒立撑') #窄距倒立撑2-12 偏重倒立撑2-10 单臂倒立撑你2-8 杠杆倒立撑2-6 单臂倒立撑2-5
+    '''
 
     # StartTrain(1, 1, 1, 10, 1, 0, '测试用')
     # 手动记录工作
     # write_record_csv(reps=20, sets=2, course='平卧抬膝', duration=3)
-    # write_record_csv(reps=10, sets=2, course='俯卧撑', duration=2)
-    # write_record_csv(reps=10, sets=2, course='引体向上', duration=2)
-
